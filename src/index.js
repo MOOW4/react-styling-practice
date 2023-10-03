a7b63cacd358790a62e9af514cd50d74f389dbac
MOOW4 <108760540+MOOW4@users.noreply.github.com>
2023-10-04 00:10:06 +0200

    add changing message besade of the time of day

--- src/index.js
+++ src/index.js
@@ -5,3 +5,32 @@
 //Apply the "heading" style in the styles.css
 //Dynamically change the color of the h1 using inline css styles.
 //Morning = red, Afternoon = green, Night = blue.
+
+import React from "react";
+import ReactDOM from "react-dom";
+let date = new Date();
+
+let colorObj = {
+    color: "blue"
+} 
+
+function setMessage() {
+	let message = "Good evening";
+	let hour = date.getHours();
+	console.log(hour);
+	if (hour > 0 && hour < 12) {
+		message = "Good Morning";
+        colorObj.color = "red;"
+	} else if (hour > 12 && hour < 18) {
+		message = "Good afternoon";
+        colorObj.color = "yellow";
+	}
+	return message;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <h1 className="heading" style={colorObj}>{setMessage()}</h1>
+);
+
+/* ReactDOM.render(<h1>{setMessage()}</h1>, document.getElementById("root")); */
